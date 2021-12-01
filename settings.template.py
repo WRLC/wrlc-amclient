4ac59e3cf3a43c2316c5031f956783c21e86c99e
Tom Boone <boone@wrlc.org>
2021-12-01 13:04:32 -0500

    update settings.template.py comments

--- settings.template.py
+++ settings.template.py
@@ -9,8 +9,8 @@ ss_user_name = 'place Storage Service username here'  # the username associated 
 
 INSTITUTION = {
     'instCode': {  # the two or three letter WRLC institution code
-        'transfer_source': '',  # the UUID of the institution's transfer source pipeline
-        'transfer_folder': '',  # the folder path in the institution's pipeline where zipped bags are stored
+        'transfer_source': '',  # the UUID of the institution's source location
+        'transfer_folder': '',  # the folder in the location where zipped bags are stored
         'transfer_type': '',  # the bag/folder type of the transfer (e.g., 'zipped bag')
         'processing_config': '',  # the name of the AM Dashboard processing config for the institution
     },
